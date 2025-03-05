3292f1955b8376e522dd0f2665d5f24ed14e1b4b
josephburgess <hello@joeburgess.dev>
2025-03-05 02:46:44 +0000

    chore: brew formula update for gust v0.0.7

--- gust.rb
+++ gust.rb
@@ -5,8 +5,8 @@
 class Gust < Formula
   desc "Command line weather app written in go"
   homepage "https://github.com/josephburgess/gust"
-  url "https://github.com/josephburgess/gust/archive/refs/tags/v0.0.6.tar.gz"
-  sha256 "5385f8e93c4785a5d58d1b9bd42392d70f0ea8569b29028a4588c8c16a4dcd8f"
+  url "https://github.com/josephburgess/gust/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "efb64472e0a3452fb4869966ba3879bf692946b961d6194da29df9683f958bc7"
 
   depends_on "go" => :build
 
